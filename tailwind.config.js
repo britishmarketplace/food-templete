af53d1f74ea185a36dfd0f11ebee24a2dca68ee9
Yeasin arafat <ayeasin108@gmail.com>
2021-11-04 06:11:37 +0600

    cart page proces

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         'banner-bg' : "url('../public/images/banner/bg_banner.jpg')",
         'login-bg' : "url('../public/images/login/login_bg.jpg')",
         'details-bg' : "url('../public/images/details/01.jpg')",
+        'cart-bg' : "url('../public/images/banner/cart_bg.png')",
       },
       height: {
         'bg-height' : '600px',
@@ -36,11 +37,13 @@ module.exports = {
     },
     maxWidth: {
       'drop-max': '250px',
+      'cart-max' : '1000px',
     },
     minHeight: {
       'min-h-log-h': '526px',
       'restItem-h' : '125px',
       'details-h' : '150px',
+      'cart-h' : '300px'
     }
   },
   variants: {
